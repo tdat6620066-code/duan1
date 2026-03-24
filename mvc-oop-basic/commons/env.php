1716b2836e8a2d8428d9635c024a12824eb2d922
tdat6620066-code <tdat6620066@gmail.com>
2026-03-24 21:42:50 +0700

    day base

---
+++ mvc-oop-basic/commons/env.php
@@ -0,0 +1,15 @@
+<?php 
+
+// Biến môi trường, dùng chung toàn hệ thống
+// Khai báo dưới dạng HẰNG SỐ để không phải dùng $GLOBALS
+
+define('BASE_URL'       , 'http://localhost/mvc-oop-basic/');
+define('BASE_URL_ADMIN' , 'http://localhost/mvc-oop-basic/admin/');
+
+define('DB_HOST'    , 'localhost');
+define('DB_PORT'    , 3306);
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+define('DB_NAME'    , 'php-oop-basic');
+
+define('PATH_ROOT'    , __DIR__ . '/../');
