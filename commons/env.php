b0b3837944011c5a155ade112da14195f69b768f
Duy-Tung7 <tunghdph40513@fpt.edu.vn>
2026-03-25 13:25:11 +0700

    update

--- commons/env.php
+++ commons/env.php
@@ -10,6 +10,6 @@ define('DB_HOST'    , 'localhost');
 define('DB_PORT'    , 3306);
 define('DB_USERNAME', 'root');
 define('DB_PASSWORD', '');
-define('DB_NAME'    , 'php-oop-basic');
+define('DB_NAME'    , 'da_banhang');
 
 define('PATH_ROOT'    , __DIR__ . '/../');
