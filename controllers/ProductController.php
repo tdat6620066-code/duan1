150be31d0a8a2a701b24ee9b9ca44d3d35bfe78b
anhhd0901 <anhhdph47285@fpt.edu.vn>
2026-04-16 23:47:29 +0700

    Sửa lại và thêm phần sản phẩm của Tú

--- controllers/ProductController.php
+++ controllers/ProductController.php
@@ -19,5 +19,28 @@ class ProductController
         $categories = $this->categoryModel->getAll();
         require_once './views/user/products.php';
     }
+    
+    public function show($id) {
+        $product = $this->productModel->getById($id);
+        if (!$product) {
+            header('Location: ' . BASE_URL . 'products');
+            exit;
+        }
+        $variants = $this->variantModel->getByProductId($id);
+        require_once './views/user/product_detail.php';
+    }
 
+    public function search() {
+        $keyword = trim($_GET['q'] ?? '');
+        $products = $this->productModel->search($keyword);
+        $categories = $this->categoryModel->getAll();
+        $searchQuery = $keyword;
+        require_once './views/user/products.php';
+    }
+
+    public function category($categoryId) {
+        $products = $this->productModel->getByCategory($categoryId);
+        $categories = $this->categoryModel->getAll();
+        require_once './views/user/products.php';
+    }
 }
